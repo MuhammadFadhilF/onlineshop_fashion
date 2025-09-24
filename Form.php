07ede86ee9033928922b0829c9656a1f6880e80c
MuhammadFadhilF <fadhilfaza08@student.uns.ac.id>
2025-09-25 00:50:37 +0700

    Form.php

--- Form.php
+++ Form.php
@@ -2,68 +2,61 @@
 class Form {
     private $action;
     private $method;
+    private $form_elements = [];
 
-    public function __construct($action = "", $method = "POST") {
+    public function __construct($action, $method = "POST") {
         $this->action = $action;
         $this->method = $method;
+    }
+
+    public function addText($name, $label) {
+        $this->form_elements[] = "<label>$label:</label><br><input type='text' name='$name'><br><br>";
+    }
+
+    public function addPassword($name, $label) {
+        $this->form_elements[] = "<label>$label:</label><br><input type='password' name='$name'><br><br>";
+    }
+
+    public function addRadio($name, $label, $options = []) {
+        $radio = "<label>$label:</label><br>";
+        foreach ($options as $opt) {
+            $radio .= "<input type='radio' name='$name' value='$opt'> $opt ";
+        }
+        $radio .= "<br><br>";
+        $this->form_elements[] = $radio;
+    }
+
+    public function addCheckbox($name, $label, $options = []) {
+        $check = "<label>$label:</label><br>";
+        foreach ($options as $opt) {
+            $check .= "<input type='checkbox' name='$name' value='$opt'> $opt ";
+        }
+        $check .= "<br><br>";
+        $this->form_elements[] = $check;
+    }
+
+    public function addDropdown($name, $label, $options = []) {
+        $select = "<label>$label:</label><br><select name='$name'>";
+        foreach ($options as $opt) {
+            $select .= "<option value='$opt'>$opt</option>";
+        }
+        $select .= "</select><br><br>";
+        $this->form_elements[] = $select;
+    }
+
+    public function addTextarea($name, $label) {
+        $this->form_elements[] = "<label>$label:</label><br><textarea name='$name'></textarea><br><br>";
+    }
+
+    public function addSubmit($name, $value) {
+        $this->form_elements[] = "<input type='submit' name='$name' value='$value'>";
+    }
+
+    public function display() {
         echo "<form action='{$this->action}' method='{$this->method}'>";
-    }
-
-    public function inputText($name, $label, $value = "") {
-        echo "<div class='mb-3'>";
-        echo "<label>{$label}</label>";
-        echo "<input type='text' name='{$name}' value='{$value}' class='form-control'>";
-        echo "</div>";
-    }
-
-    public function inputPassword($name, $label) {
-        echo "<div class='mb-3'>";
-        echo "<label>{$label}</label>";
-        echo "<input type='password' name='{$name}' class='form-control'>";
-        echo "</div>";
-    }
-
-    public function inputRadio($name, $label, $options = []) {
-        echo "<div class='mb-3'>";
-        echo "<label>{$label}</label><br>";
-        foreach ($options as $val => $text) {
-            echo "<input type='radio' name='{$name}' value='{$val}'> {$text}<br>";
+        foreach ($this->form_elements as $element) {
+            echo $element;
         }
-        echo "</div>";
-    }
-
-    public function inputCheckbox($name, $label, $options = []) {
-        echo "<div class='mb-3'>";
-        echo "<label>{$label}</label><br>";
-        foreach ($options as $val => $text) {
-            echo "<input type='checkbox' name='{$name}[]' value='{$val}'> {$text}<br>";
-        }
-        echo "</div>";
-    }
-
-    public function select($name, $label, $options = []) {
-        echo "<div class='mb-3'>";
-        echo "<label>{$label}</label>";
-        echo "<select name='{$name}' class='form-control'>";
-        foreach ($options as $val => $text) {
-            echo "<option value='{$val}'>{$text}</option>";
-        }
-        echo "</select>";
-        echo "</div>";
-    }
-
-    public function textarea($name, $label, $value = "") {
-        echo "<div class='mb-3'>";
-        echo "<label>{$label}</label>";
-        echo "<textarea name='{$name}' class='form-control'>{$value}</textarea>";
-        echo "</div>";
-    }
-
-    public function submit($label = "Simpan") {
-        echo "<button type='submit' class='btn btn-primary'>{$label}</button>";
-    }
-
-    public function close() {
         echo "</form>";
     }
 }
